34bba4e04b7d919ad837518ed792eca4d79fcf28
vandyG <vandy.goel23@gmail.com>
2025-02-24 20:31:40 -0600

    feat: add modelling package

---
+++ src/lr/modeling/training/__init__.py
@@ -0,0 +1,10 @@
+"""Linear Regression package.
+
+Implementation of Linear Regression from scratch.
+"""
+
+from __future__ import annotations
+
+from lr._internal.cli import get_parser, main
+
+__all__: list[str] = ["get_parser", "main"]
